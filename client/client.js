51cdb418c8f5f7c28bdce19f84d010892d48cd12
snailifyxd <80394595+snailifyxd@users.noreply.github.com>
2024-04-10 15:40:50 +1000

    Update client.js
    es6, added support for replies, attachments, emojis, stickers and limited message length

--- client/client.js
+++ client/client.js
@@ -1,6 +1,37 @@
-const { bridgeId, bridgeToken } = require('../config.js')
+import { bridgeId, bridgeToken } from '../config.js';
+import * as emoji from 'node-emoji';
+import emojiRegex from 'emoji-regex';
 
-async function discord(bot, client) {
+function replaceEmojisWithNames(str) {
+  const regex = emojiRegex();
+  return str.replace(regex, (match) => {
+      const name = emoji.which(match);
+      return name ? `:${name}:` : match;
+  });
+}
+
+async function formatMessage(message) {
+  if (message.stickers.size > 0) {
+    return newMsg = `:${message.stickers.first().name}:`;
+  }
+
+  let newMsg = message.content.replace(/<@(\d+)>/g, (match, userId) => {
+    const mention = message.guild.members.cache.get(userId);
+    return mention ? `@${mention.displayName}` : match;
+  }).replace(/\n/g, ' ').replaceAll(" ez ", " e.z ");
+
+  newMsg = replaceEmojisWithNames(newMsg);
+
+  if (message.attachments) {
+    message.attachments.forEach(attachment => {
+      newMsg += ` ${attachment.url}`;
+    })
+  }
+
+  return newMsg;
+}
+
+export async function discord(bot, client) {
   let bridgeChannelId;
   client.on('ready', async () => {
     console.log('Logged in as ' + client.user.tag);
@@ -11,9 +42,24 @@ async function discord(bot, client) {
   client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
     if (message.channelId != bridgeChannelId) return;
-    bot.chat(`/gc ${message.author.username}: ${message.content}`);
-    global.lastMessage = (`/gc ${message.author.username}: ${message.content}`);
+    let user = '';
+    const member = await message.guild.members.fetch(message.author.id)
+    if (message.reference && message.reference.messageId) {
+      const repliedChannel = await client.channels.cache.get(message.reference.channelId);
+      const repliedMessage = await repliedChannel.messages.fetch(message.reference.messageId);
+      const repliedContent = await formatMessage(repliedMessage);
+      if (repliedMessage.webhookId) {
+        user += `${repliedMessage.author.username}: ${repliedContent} ⤷ `;
+      }
+      else user += `${repliedMessage.member.displayName}: ${repliedContent} ⤷ `;
+    }
+    user += member.displayName;
+    const msg = await formatMessage(message);
+    let combined = user + ': ' + msg
+    if (combined.length > 240) {
+      combined = combined.substring(0,240);
+    }
+    bot.chat(`/gc ${combined}`);
+    global.lastMessage = (`/gc ${combined}`);
   });
 }
-
-module.exports = { discord };
