e5cc35b4f842e02bf633071e412985f405468c05
snailifyxd <80394595+snailifyxd@users.noreply.github.com>
2024-04-11 21:16:53 +1000

    Delete speeds.js
    its in commands dir

--- speeds.js
+++
@@ -1,4 +0,0 @@
-export default function speeds(bot) {
-  bot.chat("/gc Wart/Crops = 93% / Cocoa = 155% / Sugar Cane = 328% / Cactus = 478% (Cactus Knife) / Mushroom = 233% / Pumpkin/Melon = 290% (Updated: 04/10/24)");
-  global.lastMessage = ("/gc Wart/Crops = 93% / Cocoa = 155% / Sugar Cane = 328% / Cactus = 478% (Cactus Knife) / Mushroom = 233% / Pumpkin/Melon = 290% (Updated: 04/10/24)");
-}
