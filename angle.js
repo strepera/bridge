ca1fcf305a88ccb5df1aee73df482e71dcca035e
snailifyxd <80394595+snailifyxd@users.noreply.github.com>
2024-04-11 21:17:04 +1000

    Delete angle.js
    its in commands dir

--- angle.js
+++
@@ -1,4 +0,0 @@
-export default function angle(bot) {
-  bot.chat("/gc Wart/Crops = Yaw: 90/180 Pitch: 0/3 / Cocoa Yaw: 90/180 Pitch: 45 / Sugar Cane Yaw: 135/45 Pitch: 0 / Cactus Yaw: 90/180 Pitch: 0 / Mushroom: Yaw: 120/60 Pitch: 0 W/ Slabs or Yaw: 75 Pitch: 5.5 / Pumpkin/Melon: Yaw: 90 Pitch: -58 (Updated: 04/10/24)");
-  global.lastMessage = ("/gc Wart/Crops = Yaw: 90/180 Pitch: 0/3 / Cocoa Yaw: 90/180 Pitch: 45 / Sugar Cane Yaw: 135/45 Pitch: 0 / Cactus Yaw: 90/180 Pitch: 0 / Mushroom: Yaw: 120/60 Pitch: 0 W/ Slabs or Yaw: 75 Pitch: 5.5 / Pumpkin/Melon: Yaw: 90 Pitch: -58 (Updated: 04/10/24)");
-}
