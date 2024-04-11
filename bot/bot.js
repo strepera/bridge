30ff1a32bac8a62592e450d47c2ccff8c5ae7d8e
snailifyxd <80394595+snailifyxd@users.noreply.github.com>
2024-04-12 00:39:40 +1000

    Update bot.js
    checks for patch notes every 5 minutes and tells if advertising

--- bot/bot.js
+++ bot/bot.js
@@ -4,14 +4,44 @@ import { gameHandler, checkAnswer } from './gameHandler.js';
 
 export async function minecraft(bot, client, bridgeWebhook, logWebhook, punishWebhook) {
   gameHandler(bot);
+
+  //check for patch notes
+  let lastPatchNotes = '';
+  setInterval(async () => {
+    const response = await fetch(`https://api.hypixel.net/v2/skyblock/news?key=${process.env.apiKey}`);
+    const json = await response.json();
+    if (json.success == true) {
+      if (lastPatchNotes == '') {
+        lastPatchNotes = json.items[0].link;
+      }
+      else if (lastPatchNotes != json.items[0].link) {
+        bot.chat('/gc NEW UPDATE! ' + json.items[0].link);
+        global.lastMessage = ('/gc NEW UPDATE! ' + json.items[0].link);
+      }
+    }
+  }, 5 * 60 * 1000);
+
   bot.on('login', () => {
     console.log('Joined as ' + bot.username);
     bot.chat("§"); // send self to limbo
     bot.chat("/g online");
   })
+
   bot.on('messagestr', async (jsonMsg) => {
     if (jsonMsg.trim() == '') return;
     let match;
+
+    //check if the message was blocked
+    if (jsonMsg.match(/You cannot say the same message twice!/)) {
+      bot.chat(global.lastMessage + ' \\\\\\\\');
+      global.lastMessage = (global.lastMessage + ' \\\\\\\\');
+      return;
+    }
+    if (jsonMsg.match(/Advertising is against the rules. You will receive a punishment on the server if you attempt to advertise./)) {
+      bridgeWebhook.send(jsonMsg);
+      return;
+    }
+
     //log messages
     console.log(jsonMsg);
     logWebhook.send(jsonMsg)
@@ -23,12 +53,7 @@ export async function minecraft(bot, client, bridgeWebhook, logWebhook, punishWe
     checkAnswer(bot, jsonMsg);
 
     //minecraft -> discord handling
-    if (jsonMsg.match(/You cannot say the same message twice!/)) {
-      bot.chat(global.lastMessage + ' \\\\\\\\');
-      global.lastMessage = (global.lastMessage + ' \\\\\\\\');
-      return;
-    }
-    const regexPattern = new RegExp("Guild > (?:\\[(.+)\\] )?" + process.env.botUsername + " \\[(.+)\\]: (.+): (.+)");
+    const regexPattern = new RegExp("Guild > (?:\\[(.+)\\] )?" + process.env.botUsername + " \\[(.+)\\]: \\b(\\w+)\\b: (.+)");
     if (jsonMsg.match(regexPattern)) return;
     for (const { regex, func } of regexes) {
       if (match = jsonMsg.match(regex)) {
@@ -36,9 +61,5 @@ export async function minecraft(bot, client, bridgeWebhook, logWebhook, punishWe
         break;
       }
     };
-  bot.on('messagestr', async (jsonMsg) => {
-    if (jsonMsg.trim() == '') return;
-
-  }) 
   })
 }
