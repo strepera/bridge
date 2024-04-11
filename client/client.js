2a83196849aa01f9e8ae27d4250543c4536db6bf
snailifyxd <80394595+snailifyxd@users.noreply.github.com>
2024-04-11 21:04:53 +1000

    Update client.js
    check verification every 3 hours, verify and unverify commands, welcome and goodbye messages, say command for minecraft, i probably did something else

--- client/client.js
+++ client/client.js
@@ -1,6 +1,8 @@
-import { bridgeId, bridgeToken } from '../config.js';
 import * as emoji from 'node-emoji';
 import emojiRegex from 'emoji-regex';
+import { verifyCommand, verifyCommandData } from './commands/verify.js';
+import { unverifyCommand, unverifyCommandData } from './commands/unverify.js';
+import { checkVerification } from './checkVerification.js';
 
 function replaceEmojisWithNames(str) {
   const regex = emojiRegex();
@@ -31,13 +33,40 @@ async function formatMessage(message) {
   return newMsg;
 }
 
-export async function discord(bot, client) {
-  let bridgeChannelId;
+const commands = {
+  'verify': verifyCommand,
+  'unverify': unverifyCommand
+}
+
+export async function discord(bot, client, welcomeChannel, bridgeChannelId) {
   client.on('ready', async () => {
     console.log('Logged in as ' + client.user.tag);
-    const response = await fetch(`https://discord.com/api/webhooks/${bridgeId}/${bridgeToken}`);
+    const guild = await client.guilds.fetch(process.env.guildId);
+    await guild.commands.create(verifyCommandData);
+    await guild.commands.create(unverifyCommandData);
+    await guild.commands.create({
+      name: "say",
+      description: "for snail",
+      options: [{
+          name: "message",
+          type: "STRING",
+          description: "message",
+          required: true
+      }]
+    })
+    const response = await fetch(`https://discord.com/api/webhooks/${process.env.bridgeId}/${process.env.bridgeToken}`);
     const json = await response.json();
+    welcomeChannel = client.channels.cache.get(process.env.welcomeChannelId);
     bridgeChannelId = json.channel_id;
+    setInterval(() => {
+      client.user.setActivity(`${global.onlinePlayers} players!`, {type: "WATCHING"});
+    }, 60 * 1000);
+    setInterval(() => {
+      guild.members.fetch()
+          .then(members => {
+              members.forEach(member => checkVerification(member))
+          })
+    }, 3 * 60 * 60 * 1000);
   });
   client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
@@ -61,5 +90,24 @@ export async function discord(bot, client) {
     }
     bot.chat(`/gc ${combined}`);
     global.lastMessage = (`/gc ${combined}`);
+  });
+  client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    const { commandName, options } = interaction;
+    if (commandName == 'say') {
+      if (interaction.user.id != process.env.ownerId) return;
+      bot.chat(options.getString('message'));
+    }
+    else {
+      commands[commandName](interaction, options.getString('username'));
+    }
+  })
+  client.on('guildMemberAdd', async(member) => {
+    await welcomeChannel.send('Welcome <@' + member.user.id + '>!');
+    checkVerification(member);
+  });
+  
+   client.on('guildMemberRemove', async(member) => {
+    await welcomeChannel.send('Goodbye ' + member.user.username);
   });
 }
