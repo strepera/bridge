f91b1eeef0d0ace82cb5004b33413fa4b152fb63
snailifyxd <80394595+snailifyxd@users.noreply.github.com>
2024-04-12 18:40:33 +1000

    Update gameHandler.js
    actually imports automatically, just export a check function in the game file

--- bot/gameHandler.js
+++ bot/gameHandler.js
@@ -1,59 +1,43 @@
 import * as fs from 'fs';
 
+const commands = [];
+
 export async function gameHandler(bot) {
   fs.readdir('bot/games', async (err, files) => {
     if (err) {
       console.error(err);
       return;
     }
-    const commands = [];
     for (const file of files) {
       const imported = await import (`./games/${file}`)
-      commands.push(imported.default || imported);
+      commands.push(imported);
     }
     for (const command of commands) {
       const frequency = process.env.gameFrequency * 60 * 1000;
       setTimeout(() => {
         setInterval(() => {
-          command(bot);
+          command.default(bot);
         }, frequency);
       }, frequency * commands.indexOf(command) / commands.length );
-      console.log(command, frequency, commands.length)
+      console.log('Imported:');
+      console.log(command)
     }
   })
 }
 
 export async function checkAnswer(bot, jsonMsg) {
     let match;
-    const regexPattern = new RegExp("Guild > (?:\\[(\\w+\\+?)\\] )?" + process.env.botUsername + " \\[(\\w+)\\]: .*");
-    const regexPatternWithoutMessage = new RegExp("Guild > (?:\\[(\\w+\\+?)\\] )?" + process.env.botUsername + " \\[(\\w+)\\]: (.+) .*");
-    if (jsonMsg.match(regexPattern) && !jsonMsg.match(regexPatternWithoutMessage)) return;
+    const mcRegex = new RegExp("Guild > (?:\\[(\\w+\\+?)\\] )?" + process.env.botUsername + " \\[(\\w+)\\]: .*");
+    const dcRegex = new RegExp("Guild > (?:\\[(\\w+\\+?)\\] )?" + process.env.botUsername + " \\[(\\w+)\\]: (.+) .*");
+    if (jsonMsg.match(mcRegex) && !jsonMsg.match(dcRegex)) return;
     if (match = jsonMsg.match(/Guild > (?:\[(\w+\+?)\] )?(\w+) \[(\w+)\]: (.+): (.*)/)) {
-      if (match[5] == global.mathAnswer) {
-        const elapsedTime = Date.now() - global.mathAnswerTimestamp;
-        bot.chat(`/gc ${match[4]} got it correct in ${elapsedTime} ms!`);
-        global.lastMessage = (`/gc ${match[2]} got it correct in ${elapsedTime} ms!`);
-        global.mathAnswer = null;
-      }
-      else if (match[5].toLowerCase().includes(global.randomItemName)) {
-        const elapsedTime = Date.now() - global.randomItemNameTimestamp;
-        bot.chat(`/gc ${match[4]} got it correct in ${elapsedTime} ms!`);
-        global.lastMessage = (`/gc ${match[4]} got it correct in ${elapsedTime} ms!`);
-        global.randomItemName = null;
+      for (const command of commands) {
+        command.check(match[5], match[4], bot);
       }
     }
     else if (match = jsonMsg.match(/Guild > (?:\[(\w+\+?)\] )?(\w+) \[(\w+)\]: (.*)/)) {
-      if (match[4] == global.mathAnswer) {
-        const elapsedTime = Date.now() - global.mathAnswerTimestamp;
-        bot.chat(`/gc ${match[2]} got it correct in ${elapsedTime} ms!`);
-        global.lastMessage = (`/gc ${match[2]} got it correct in ${elapsedTime} ms!`);
-        global.mathAnswer = null;
-      }
-      else if (match[4].toLowerCase().includes(global.randomItemName)) {
-        const elapsedTime = Date.now() - global.randomItemNameTimestamp;
-        bot.chat(`/gc ${match[2]} got it correct in ${elapsedTime} ms!`);
-        global.lastMessage = (`/gc ${match[2]} got it correct in ${elapsedTime} ms!`);
-        global.randomItemName = null;
+      for (const command of commands) {
+        command.check(match[4], match[2], bot);
       }
     }
 }
