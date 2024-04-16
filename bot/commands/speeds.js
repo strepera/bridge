0376c1ae7a38e945ebb24733340630b53f29345a
snailify <80394595+snailifyxd@users.noreply.github.com>
2024-04-16 16:42:48 +1000

    Update speeds.js
    added fuzions changes but with more aliases and better looking messages with the angle aswell

--- bot/commands/speeds.js
+++ bot/commands/speeds.js
@@ -1,4 +1,43 @@
-export default function speeds(bot) {
-  bot.chat("/gc Wart/Crops = 93% / Cocoa = 155% / Sugar Cane = 328% / Cactus = 478% (Cactus Knife) / Mushroom = 233% / Pumpkin/Melon = 290% (Updated: 04/10/24)");
-  global.lastMessage = ("/gc Wart/Crops = 93% / Cocoa = 155% / Sugar Cane = 328% / Cactus = 478% (Cactus Knife) / Mushroom = 233% / Pumpkin/Melon = 290% (Updated: 04/10/24)");
+export default function speeds(bot, requestedCategory) {
+  let message;
+  const aliases = {
+    "crops": ["crops", "nether wart", "wart", "carrot", "potato", "wheat"],
+    "sugarcane": ["cane", "sugar cane"],
+    "cocoa": ["cocoa", "bean", "beans", "cocoa bean", "cocoa beans"],
+    "cactus": ["cactus"],
+    "mushroom": ["mushroom", "shroom"],
+    "melon": ["melon"],
+    "pumpkin": ["pumpkin"]
+  };
+
+  const category = Object.keys(aliases).find(key => aliases[key].includes(requestedCategory.toLowerCase()));
+
+  switch (category) {
+    case "crops":
+      message = "/gc " + requestedCategory + " (✦93) (⇄90/180) (⇅0/3)";
+      break;
+    case "sugarcane":
+      message = "/gc Sugar Cane (✦328) (⇄135/45) (⇅0)";
+      break;
+    case "cocoa":
+      message = "/gc Cocoa Beans (✦155 ✖Sprint) (⇄90/180) (⇅45)";
+      break;
+    case "cactus":
+      message = "/gc Cactus (✦478) (⇄90/180) (⇅0)";
+      break;
+    case "mushroom":
+      message = "/gc Sugar Cane (✦233) (⇄120/60) (⇅0)";
+      break;
+    case "melon":
+      message = "/gc Melon (✦290) (⇄90/180) (⇅58/-58)";
+      break;
+    case "pumpkin":
+      message = "/gc Pumpkin (✦290) (⇄90/180) (⇅58/-58)";
+      break;
+    default:
+      message = "/gc Invalid category. Use .speeds {crops/cane/cocoa/cactus/mushroom/melon/pumpkin}.";
+      break;
+  }
+  bot.chat(message);
+  global.lastMessage = message;
 }
