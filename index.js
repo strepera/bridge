d3842398899d7458de90b47d8b8412d28a51061e
snailify <80394595+snailifyxd@users.noreply.github.com>
2024-04-16 22:01:22 +1000

    Update index.js
    forgot to update for env

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const { username, token, bridgeToken, bridgeId, logToken, logId } = require('./config.js'); 
-const { minecraft } = require('./bot/bot.js');
-const { discord } = require('./client/client.js');
+import { minecraft } from './bot/bot.js';
+import { discord } from './client/client.js';
+import mineflayer from 'mineflayer';
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
 
-const mineflayer = require('mineflayer');
 const bot = mineflayer.createBot({
   host: "mc.hypixel.net",
-  username: username,
+  username: "micahkoritz666@gmail.com",
   auth: "microsoft",
   port: "25565",
   version: "1.8.9"
 });
 
-const { Client, Intents, WebhookClient } = require('discord.js');
+import { Client, Intents, WebhookClient } from 'discord.js';
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.MESSAGE_CONTENT, Intents.FLAGS.GUILD_MEMBERS] });
-const bridgeWebhook = new WebhookClient({ id: bridgeId, token: bridgeToken})
-const logWebhook = new WebhookClient({ id: logId, token: logToken})
+const bridgeWebhook = new WebhookClient({ id: process.env.bridgeId, token: process.env.bridgeToken})
+const logWebhook = new WebhookClient({ id: process.env.logId, token: process.env.logToken})
+const punishWebhook = new WebhookClient({ id: process.env.punishId, token: process.env.punishToken})
 
 global.lastMessage = '';
+global.onlinePlayers = 0;
+global.totalPlayers = 0;
 
-minecraft(bot, client, bridgeWebhook, logWebhook);
+minecraft(bot, client, bridgeWebhook, logWebhook, punishWebhook);
 discord(bot, client);
 
-client.login(token);
+client.login(process.env.token);
