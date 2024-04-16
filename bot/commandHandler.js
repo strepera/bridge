a833bcc3c7770655f4c8b2c13ba6de1ac80f2f48
snailify <80394595+snailifyxd@users.noreply.github.com>
2024-04-16 22:16:57 +1000

    Update commandHandler.js
    works with mvp++ for real

--- bot/commandHandler.js
+++ bot/commandHandler.js
@@ -30,7 +30,7 @@ const aliases = {
 }
 
 export default async function commands(bot, jsonMsg, match) {
-if (match = jsonMsg.match(/Guild > (?:\[(\w+\+?)\] )?(\w+) \[(\w+)\]: \.(\w+)( .*)?/)) {
+if (match = jsonMsg.match(/Guild > (?:\[(.+\+?)\] )?(\w+) \[(\w+)\]: \.(\w+)( .*)?/)) {
     let command = match[4];
     let requestedPlayer = match[5] || match[2];
     if (requestedPlayer.split('')[0] == ' ') requestedPlayer = requestedPlayer.substring(1);
@@ -44,7 +44,7 @@ if (match = jsonMsg.match(/Guild > (?:\[(\w+\+?)\] )?(\w+) \[(\w+)\]: \.(\w+)( .
         const executed = await getCommandAliases(command) || await importCommand(command);
         if (executed) executed(bot, requestedPlayer);
     }
-} else if (match = jsonMsg.match(/Guild > (?:\[(\w+\+?)\] )?(\w+) \[(\w+)\]: (.+): \.(\w+)( .*)?/)) {
+} else if (match = jsonMsg.match(/Guild > (?:\[(.+\+?)\] )?(\w+) \[(\w+)\]: (.+): \.(\w+)( .*)?/)) {
     let command = match[5];
     let requestedPlayer = match[6] || match[4];
     if (requestedPlayer.split('')[0] == ' ') requestedPlayer = requestedPlayer.substring(1);
