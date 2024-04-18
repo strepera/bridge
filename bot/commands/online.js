5c293df0166ba8593437d78038bd1c82070c1adf
snailify <80394595+snailifyxd@users.noreply.github.com>
2024-04-19 00:41:32 +1000

    Update online.js
    made it look prettier or something

--- bot/commands/online.js
+++ bot/commands/online.js
@@ -1,20 +1,39 @@
+const readability = {
+  'farming_1': 'Barn Islands',
+  'foraging_1': 'Park',
+  'mining_1': 'Gold Mine',
+  'mining_2': 'Deep Caverns',
+  'mining_3': 'Dwarven Mines',
+  'combat_1': "Spider's Den",
+  'combat_3': 'End',
+  'dynamic': 'Private Island'
+}
+
 export default async function(bot, requestedPlayer) {
   let uuid;
   try {
-   const response1 = await fetch(`https://api.mojang.com/users/profiles/minecraft/${requestedPlayer}`);
-   const json1 = await response1.json();
-   uuid = json1.id;
-   const response2 = await fetch(`https://api.hypixel.net/status?key=${process.env.apiKey}&uuid=${uuid}`);
-   const json2 = await response2.json();
-   if (json2.success === true) {
-     let online = json2.session.online;
-     let gameType = json2.session.gameType;
-     let mode = json2.session.mode;
-     mode = mode === "dynamic" ? "Private island" : mode;
-     bot.chat(online ? `/gc ${requestedPlayer} is in ${gameType} - ${mode}` : `${requestedPlayer} is offline.`);
-     global.lastMessage = (online ? `/gc ${requestedPlayer} is in ${gameType} - ${mode}` : `${requestedPlayer} is offline.`);
-   }
+      const response1 = await fetch(`https://api.mojang.com/users/profiles/minecraft/${requestedPlayer}`);
+      const json1 = await response1.json();
+      uuid = json1.id;
+      const response2 = await fetch(`https://api.hypixel.net/status?key=${process.env.apiKey}&uuid=${uuid}`);
+      const json2 = await response2.json();
+      if (json2.success === true) {
+          let online = json2.session.online;
+          let gameType = json2.session.gameType;
+          let mode = json2.session.mode;
+          if (readability[mode]) {
+              mode = readability[mode];
+          }
+          mode = mode.replaceAll('_', ' ');
+          let modeParts = mode.split(' ');
+          for (let part in modeParts) {
+              modeParts[part] = modeParts[part][0].toUpperCase() + modeParts[part].slice(1);
+          }
+          mode = modeParts.join(' ');
+          bot.chat(online ? `/gc ${requestedPlayer} is in ${gameType} - ${mode}` : `${requestedPlayer} is offline.`);
+          global.lastMessage = (online ? `/gc ${requestedPlayer} is in ${gameType} - ${mode}` : `${requestedPlayer} is offline.`);
+      }
   } catch (error) {
-   console.error('Error:', error);
+      console.error('Error:', error);
   }
 }
