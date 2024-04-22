dfa24c73e1040bfc1a43c587d1972e2af2bfec68
FUZION <fuzionplaysrbx@gmail.com>
2024-04-22 07:41:43 -0400

    please dont break

--- bot/commands/dice.js
+++ bot/commands/dice.js
@@ -1,17 +1,25 @@
 import fs from 'fs';
 
 export default async function(bot, bet, player, chat) {
-    bet = bet.split(' ')[0].replace(/[^0-9]/g, '');
-    if (bet.trim() == '') {
+    let betAmount;
+    if (bet.trim().toLowerCase() === 'all') {
+        betAmount = playerObj.coins;
+    } else {
+        betAmount = bet.split(' ')[0].replace(/[^0-9]/g, '');
+    }
+
+    if (betAmount.trim() == '') {
         bot.chat(chat + 'You need to bet an amount! e.g. ".dice 100"');
         global.lastMessage = (chat + 'You need to bet an amount! e.g. ".dice 100"');
         return;
     }
-    if (Number(bet) < 100) {
-        bot.chat(chat + 'You need to bet at least 100 coins!');
-        global.lastMessage = (chat + 'You need to bet at least 100 coins!');
+
+    if (Number(betAmount) < 100) {
+        bot.chat(chat + 'You need to bet at least 100 coins! You have ' + playerObj.coins + ' coins.');
+        global.lastMessage = (chat + 'You need to bet at least 100 coins! You have ' + playerObj.coins + ' coins.');
         return;
     }
+
     let playerObj;
     const data = await fs.promises.readFile('bot/playerData.json', 'utf8');
     let json = JSON.parse(data);
