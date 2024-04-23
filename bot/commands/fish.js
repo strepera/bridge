298f3022a0875ed4ad5eb0cbd77c454148082306
snailify <80394595+snailifyxd@users.noreply.github.com>
2024-04-23 22:11:59 +1000

    Update fish.js
    wow fishing command that fishes

--- bot/commands/fish.js
+++ bot/commands/fish.js
@@ -1,5 +1,87 @@
 import fs from 'fs';
 
+function ranRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const worthList = {
+    'fish': () => {return ranRange(20, 100)},
+    'creature': () => {return ranRange(200, 500)},
+    'trash': () => {return ranRange(1, 5)},
+    'jackpot': () => {return ranRange(2000, 3000)},
+    'superpot': () => {return ranRange(5000, 10000)}
+}
+
+const nameList = {
+    'fish': ['Salmon', 'Cod', 'Clownfish', 'Squid', 'Eel', 'Pufferfish'],
+    'creature': ['Sea Walker', 'Sea Guardian', 'Sea Witch', 'Sea Archer', 'Rider of the Deep', 'Catfish'],
+    'trash': ['Seaweed', 'Old Boot', 'Empty Tin Can'],
+    'jackpot': ['Jackpot'],
+    'superpot': ['Super Jackpot']
+}
+
 export default async function fish(bot, request, player, chat) {
-    
+
+    function msg(message) {
+        bot.chat(chat + message);
+        global.lastMessage = (chat + message);
+    }
+
+    if (!global.fishCooldown) global.fishCooldown = 0;
+
+    const cooldown = Date.now() - global.fishCooldown;
+
+    if (cooldown < 10 * 60 * 1000) {
+        const remainingSeconds = Math.ceil((10 * 60 * 1000 - cooldown) / 1000);
+        msg(`The pond is empty! Please wait ${remainingSeconds} seconds to fish again.`);
+        return;
+    }
+
+    global.fishCooldown = Date.now();
+
+    const fishAmount = ranRange(5, 7);
+    msg("Fishing " + fishAmount + ' times...');
+    let results = [];
+
+    for (let i = 0; i < fishAmount; i++) {
+        const randomValue = Math.random();
+        let type;
+        if (randomValue < 0.6) {
+            type = 'fish';
+        } else if (randomValue < 0.84) {
+            type = 'creature';
+        } else if (randomValue < 0.9968) {
+            type = 'trash';
+        } else if (randomValue < 0.9976) {
+            type = 'jackpot';
+        } else {
+            type = 'superpot';
+        }
+        results.push({
+            type: type,
+            worth: worthList[type](),
+            name: nameList[type][ranRange(0, nameList[type].length - 1)]
+        })
+    }
+
+    let message = [];
+    let total = 0;
+    for (const result of results) {
+        message.push(result.name + ' [' + result.worth + ']');
+        total += result.worth;
+    }
+    setTimeout(() => {
+        msg("You caught: " + message.join(' '));
+        setTimeout(() => {
+            msg("Total: $" + total);
+        }, 500);
+    }, 1000);
+
+    const data = await fs.promises.readFile('bot/playerData.json', 'utf8');
+    const json = JSON.parse(data);
+    const playerObj = json[player.toLowerCase()];
+    playerObj.coins += total;
+
+    fs.writeFileSync('bot/playerData.json', JSON.stringify(json, null, 2));
+
 }
